cf5ad1e552019d345e655f1f8b12bfac6e409d90
Philip Karnatsevich <kapitoshcka@gmail.com>
2024-12-10 23:13:36 -0800

    Initial Commit

---
+++ WizeGatorApp/WizeGatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizeGatorAppApp.swift
+//  WizeGatorApp
+//
+//  Created by Philip Karnatsevich on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WizeGatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
